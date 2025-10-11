10a9241399cd9c95c5675b31d9d116f44d390eb8
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-10-11 23:36:53 +0300

    Sign Up completed

---
+++ frontend/src/pages/SignUp/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export function signUp(body)
+{
+    return axios.post('/api/v1/users',body);
+}
--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,71 +1,110 @@
 import { useState } from "react";
-import axios from "axios";
+import { signUp } from "./api";
 
 export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+  const [apiProgress, setApiProgress] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
 
-    const [username , setUsername] = useState();
-    const [email , setEmail] = useState();
-    const [password , setPassword] = useState();
-    const [passwordRepeat , setPasswordRepeat] = useState();
-    const [apiProgress,setApiProgress] = useState(false);
-    const [successMessage,setSuccessMessage] = useState(false);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setSuccessMessage();
+    setApiProgress(true);
 
-    const onSubmit=(event) =>
-      {
-        event.preventDefault();//event'in browser tarafından proses edilmesini engelliyor.
-        setSuccessMessage();
-        setApiProgress(true);
-        axios.post('/api/v1/users',{
-          username,
-          email,
-          password
-        })
-        .then((response)=>{
-          setSuccessMessage(response.data.message)
-        })
-        .finally(()=> setApiProgress(false))
-      }
+    try {
+      const response = await signUp({
+        username,
+        email,
+        password,
+      });
+      setSuccessMessage(response.data.message);
+    } catch {
+      /* empty */
+    } finally {
+      setApiProgress(false);
+    }
+
+  };
 
   return (
     <div className="container">
       <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
-    <form  className="card" onSubmit={onSubmit}>
-      <div className="text-center card-header">
-      <h1>Sign Up</h1>
-      </div>
-      <div className="card-body">
-      <div className="mb-3">
-        <label htmlFor="username" className="form-label">Username</label>
-        <input id="username"  className="form-control" value={username} onChange={(event) => setUsername(event.target.value)}/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">Email</label>
-        <input id="email"   className="form-control" value={email} onChange={(event) => setEmail(event.target.value)} />
-      </div>
-       <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input id="password"  className="form-control" type="password" onChange={(event) => setPassword(event.target.value)}/>
-      </div>
-        <div className="mb-3">
-        <label htmlFor="passwordRepeat" className="form-label">Password Repeat</label>
-        <input id="passwordRepeat" className="form-control" type="password" onChange={(event) => setPasswordRepeat(event.target.value)}/>
-      </div>
+        <form className="card" onSubmit={onSubmit}>
+          <div className="text-center card-header">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                id="username"
+                className="form-control"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                id="email"
+                className="form-control"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                id="password"
+                className="form-control"
+                type="password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordRepeat" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                id="passwordRepeat"
+                className="form-control"
+                type="password"
+                onChange={(event) => setPasswordRepeat(event.target.value)}
+              />
+            </div>
 
-      {successMessage && <div className="alert alert-success">{successMessage}</div>}
+            {successMessage && (
+              <div className="alert alert-success">{successMessage}</div>
+            )}
 
-      <div className="text-center">
-     <button
-      className="btn btn-primary"
-       disabled={apiProgress || (!password || password !== passwordRepeat)}
-      >
-      {apiProgress && 
-      <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
-        Sign Up
-        </button>
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={
+                  apiProgress || !password || password !== passwordRepeat
+                }
+              >
+                {apiProgress && (
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                  ></span>
+                )}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      </div>
-    </form>
-    </div>
     </div>
   );
 }
