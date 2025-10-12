82994e68d884b79e29bacf7e4c6bbc8b1c85fe5a
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-10-12 13:30:19 +0300

    update

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signUp } from "./api";
 
 export function SignUp() {
@@ -8,10 +8,17 @@ export function SignUp() {
   const [passwordRepeat, setPasswordRepeat] = useState();
   const [apiProgress, setApiProgress] = useState(false);
   const [successMessage, setSuccessMessage] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [generalerror , setGeneralError] = useState();
+
+  useEffect(()=>{
+    setErrors({})
+  },[username])
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setSuccessMessage();
+    setGeneralError();
     setApiProgress(true);
 
     try {
@@ -21,12 +28,16 @@ export function SignUp() {
         password,
       });
       setSuccessMessage(response.data.message);
-    } catch {
-      /* empty */
+    } catch (axiosError) {
+      if (axiosError.response?.data && 
+        axiosError.response.data.status == 400) {
+        setErrors(axiosError.response.data.validationErrors);
+      } else {
+        setGeneralError("Unexpected error occured. Please try again");
+      }
     } finally {
       setApiProgress(false);
     }
-
   };
 
   return (
@@ -43,11 +54,14 @@ export function SignUp() {
               </label>
               <input
                 id="username"
-                className="form-control"
+                className={
+                  errors.username ? "form-control is-invalid" : "form-control"
+                }
                 value={username}
                 onChange={(event) => setUsername(event.target.value)}
               />
             </div>
+            <div className="invalid-feedback">{errors.username}</div>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">
                 Email
@@ -84,6 +98,10 @@ export function SignUp() {
 
             {successMessage && (
               <div className="alert alert-success">{successMessage}</div>
+            )}
+
+             {generalerror && (
+              <div className="alert alert-danger">{generalerror}</div>
             )}
 
             <div className="text-center">
--- ws/src/main/java/com/example/ws/user/UserController.java
+++ ws/src/main/java/com/example/ws/user/UserController.java
@@ -1,11 +1,16 @@
 package com.example.ws.user;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ws.error.ApiError;
 import com.example.ws.shared.GenericMessage;
 
 @RestController
@@ -15,10 +20,31 @@ public class UserController
    UserService userService;
 
     @PostMapping("/api/v1/users")
-    GenericMessage creatUser(@RequestBody User user)
+    ResponseEntity<?> creatUser(@RequestBody User user)
     {
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+         Map<String,String> validationErrors = new HashMap<>();
+        if(user.getUsername() == null || user.getUsername().isEmpty())
+        {
+             validationErrors.put("username", "Username cannot be null");
+        }
+
+        if(user.getEmail() == null || user.getEmail().isEmpty())
+        {
+            validationErrors.put("email", "Email cannot be null");
+        }
+           
+        if(validationErrors.size() > 0 ){
+         apiError.setValidationErrors(validationErrors);
+         return ResponseEntity.badRequest().body(apiError);
+        }
+        
+        
         userService.save(user);
-        return new GenericMessage("User is created");
+        return ResponseEntity.ok(new GenericMessage("User is created"));
     }
 
 }
